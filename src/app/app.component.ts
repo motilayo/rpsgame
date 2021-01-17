b50cd90270b2ceb0a05b626aa874b1a307dd57d3
Josh-Agboola <44736801+Josh-Agboola@users.noreply.github.com>
2021-01-16 20:45:01 -0500

    commit on main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rpsgame';
+  title = 'Rock Paper Scissors Game';
+  username: string;
+  userScore= 0;
+  computerScore = 0;
+  options = ["rock", "paper", "scissors"]
+  userChoice: string;
+  computerChoice: string;
+  outcome: string;
+  
+
+  setComputerChoice(){
+    const index = Math.floor(Math.random() * 2)
+    this.computerChoice = this.options[index]
+  }
+
+  userChoose(choice: string){
+    this.setComputerChoice()
+    this.userChoice = choice
+    console.log(this.computerChoice)
+    this.evaluate()
+  }
+
+  evaluate(){
+    switch(this.userChoice+this.computerChoice){
+      case 'rockpaper':
+        this.outputA()
+        break;
+      case 'rockscissors':
+        this.outputB()
+        break;
+      case 'paperrock':
+        this.outputB();
+        break;
+      case 'paperscissors':
+        this.outputA();
+        break;
+      case 'scissorsrock':
+        this.outputA();
+        break;
+      case 'scissorspaper':
+        this.outputB();
+        break;
+      default:
+        this.outcome = 'DRAW';
+        this.clear();
+    }
+  }
+
+  outputA(){
+    this.computerScore ++;
+    this.outcome = 'You Lose!';
+    this.clear();
+  }
+
+  outputB(){
+    this.userScore ++;
+    this.outcome = 'You win!';
+    this.clear();
+  }
+
+  clear(){
+    setTimeout(() => {
+      this.userChoice = '';
+      this.computerChoice = '';
+      this.outcome = '';
+    }, 1500);
+  }
 }
